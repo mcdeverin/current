0bd637d0373193c03684b0676f4112636580ac78
Macauley Deverin <mcdeverin@gmail.com>
2026-03-11 19:21:52 -0400

    Add Capacitor iOS with geolocation and push notifications

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'studio.currentapp',
+  appName: 'Current',
+  webDir: 'dist'
+};
+
+export default config;
